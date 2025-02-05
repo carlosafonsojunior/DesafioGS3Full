38f8c2630d0f655c03c8cd146e96336418b13207
carlos junior <67748031+carlosafonsojunior@users.noreply.github.com>
2025-02-05 18:28:50 -0300

    Create user.service.ts

---
+++ user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = 'http://localhost/api/users';
+
+  constructor(private http: HttpClient) { }
+
+  getUser(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
+
+  updateUser(id: number, data: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
+  }
+}
