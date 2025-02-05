bb01cf34219c99d4d838e4c2daf1e48094e7bced
carlos junior <67748031+carlosafonsojunior@users.noreply.github.com>
2025-02-05 18:35:52 -0300

    Create USerController.php

---
+++ USerController.php
@@ -0,0 +1,28 @@
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\Profile;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function store(Request $request)
+    {
+        $user = User::create($request->only(['name', 'email', 'password']));
+        $user->profiles()->attach($request->profile_id);
+        return response()->json($user);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $user->update($request->only(['name', 'email', 'password']));
+        $user->profiles()->sync($request->profile_id);
+        return response()->json($user);
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return response()->json(null, 204);
+    }
+}
